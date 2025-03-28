602b364efe0c0a0b16066314fdfe89428e3e294d
Valentine Omonya <valomosh254@gmail.com>
2025-03-28 22:45:37 +0300

    Updated register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -70,7 +70,7 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="container flex items-center justify-center min-h-screen py-12">
+    <div className="mx-auto container  flex items-center justify-center min-h-screen py-12">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl font-bold">Create an account</CardTitle>
@@ -149,4 +149,3 @@ export default function RegisterPage() {
     </div>
   )
 }
-
